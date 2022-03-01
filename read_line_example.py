2f3fb37a6b24e1dc621666beac4f37403677e2e8
Ioannis Gallios <ig29150@uga.edu>
2022-03-01 11:57:27 -0500

    update

--- read_line_example.py
+++ read_line_example.py
@@ -1,21 +1,22 @@
-file_handle = open("test.txt")
-lines = file_handle.readlines()
+file_handle = open("data/test.txt")
+lines = [x.strip() for x in file_handle.readlines()]
 print(lines)
 file_handle.close()
 
-file_handle = open("test.txt")
-file_data= file_handle.read()
-file_lines = file_data.split("#")
+file_handle = open("data/test.txt")
+file_data = file_handle.read()
+file_lines = file_data.split("\n")
 print(file_lines)
 file_handle.close()
 
-file_handle = open("test.txt")
-numbers = []
+file_handle = open("data/test.txt")
+file_lines = []
 while True:
     line = file_handle.readline()
-    if line == "":break
-    numbers.append(line.strip())
-print(numbers)
+    if line == "": 
+        break
+    file_lines.append(line.strip())
+print(file_lines)
 file_handle.close()
 
 
